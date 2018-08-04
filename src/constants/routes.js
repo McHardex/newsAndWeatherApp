b00fc8885f45468000fca3bdd794cc260d82fd47
mchardex <bukunmiadebisi@gmail.com>
2018-08-04 22:16:07 +0100

    adjusted router

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,5 @@
 export const SIGN_UP = '/signup';
-export const SIGN_IN = '/';
-export const HOME = '/home';
+export const SIGN_IN = '/home';
+export const HOME = '/';
 export const ACCOUNT = '/account';
 export const PASSWORD_FORGET = '/pw-forget';
