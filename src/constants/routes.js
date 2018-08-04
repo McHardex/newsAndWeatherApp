fbac552ad73a5962ead0676c1e46c24d8d1c7421
mchardex <bukunmiadebisi@gmail.com>
2018-08-04 22:41:48 +0100

    adjusted router

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,5 @@
 export const SIGN_UP = '/signup';
-export const SIGN_IN = '/';
+export const SIGN_IN = '/weatherAndNewsApp';
 export const HOME = '/home';
 export const ACCOUNT = '/account';
 export const PASSWORD_FORGET = '/pw-forget';
