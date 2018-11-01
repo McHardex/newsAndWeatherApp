c14320fb71ff29b9ab09223e78a36cd75f9ee14b
mchardex <mchardex@gmail.com>
2018-11-01 02:14:16 +0100

    adjusting authorisation

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,5 @@
-export const SIGN_UP = '/signup';
-export const SIGN_IN = '/';
+export const SIGN_UP = '/';
+export const SIGN_IN = '/sign-in';
 export const HOME = '/home';
 export const ACCOUNT = '/account';
 export const PASSWORD_FORGET = '/pw-forget';
