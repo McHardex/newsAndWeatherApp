163af4f755edfaa6bb94ce1d91ff1ff46d70342c
mchardex <mchardex@gmail.com>
2018-11-22 14:24:06 +0100

    update styling

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,5 @@
-export const SIGN_UP = '/sign-up';
+export const SIGN_UP = 'sign-up';
 export const SIGN_IN = '/';
-export const HOME = '/home';
-export const ACCOUNT = '/account';
-export const PASSWORD_FORGET = '/pw-forget';
+export const HOME = 'home';
+export const ACCOUNT = 'account';
+export const PASSWORD_FORGET = 'pw-forget';
